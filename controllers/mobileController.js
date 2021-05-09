0047b3419868775b415f79decf128fd2c069d819
Shubham <smartshubhampathak@gmail.com>
2021-05-09 14:05:36 +0530

    added a mobile view

--- controllers/mobileController.js
+++ controllers/mobileController.js
@@ -1,6 +1,7 @@
 const express = require('express')
 var router = express.Router()
 const blogPosts = require('../models/blogPosts')
+const comment = require('../models/comment');
 const path = require('path');
 
 //Serving static files
@@ -35,6 +36,47 @@ router.get('/blogPosts', function (req, res) {
 
 })
 
+
+router.use('/single/:postID', express.static('public'));
+router.get('/single/:postID',(req, res)=> {
+
+    //get all tags and recent blogs
+    //get single articles and its comments using id
+    blogPosts.findById(req.params.postID, (err, post) => {
+        if (err) console.log(err);
+
+        //Updating the views of the post as someone visited the page
+        post.views += 1;
+        post.save((err) => {
+            if (err) console.log(err);
+        });
+
+        //fetching comments of this post
+        comment.find({ _postid: req.params.postID }, (err, comments) => {
+
+
+            //fetching recent Blogs
+            blogPosts.find(async (err, recentPosts) =>{
+                if (err) console.log(err);
+
+                //finally rendering the article page
+                return res.render('mobileView', {
+                    post: post,
+                    comments: comments,
+                    recentPosts: recentPosts,
+                });
+
+            }).select('_id title timestamp imageurl slug timeToRead')
+                .sort({ timestamp: 'desc' })
+                .limit(3);
+
+        }).sort({ timestamp: 'desc' })
+            .select('-_postid')
+    })
+        .select('_id title body author shortDescription aboutAuthor imageurl tags views slug')
+})
+
+
 //sending images to mobile application
 router.get('/:imageurl', function (req, res) {
     //get the image of a particular post
@@ -42,5 +84,6 @@ router.get('/:imageurl', function (req, res) {
     var imagepath = path.join(__dirname, '../public/uploads/' + req.params.imageurl)
     res.sendFile(imagepath)
 })
+
 
 module.exports = router
